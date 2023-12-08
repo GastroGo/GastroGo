978d6a328268aa596cd15764ea1b7c3f54b76d14
Martin <130224348+mt1nr@users.noreply.github.com>
2023-12-08 10:32:10 +0100

    zwischendings

--- app/src/main/java/com/example/login/CreateRestaurant.java
+++ app/src/main/java/com/example/login/CreateRestaurant.java
@@ -3,13 +3,17 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateRestaurant extends AppCompatActivity {
 
@@ -26,6 +30,7 @@ public class CreateRestaurant extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_restaurant);
         mAuth = FirebaseAuth.getInstance();
+        dbRef = FirebaseDatabase.getInstance().getReference("Restaurant");
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
         editTextStreet = findViewById(R.id.street);
@@ -33,5 +38,24 @@ public class CreateRestaurant extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar);
         textView1 = findViewById(R.id.loginNow);
         textView2 = findViewById(R.id.registerNow);
+
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
+
 }
