71c0841b1e822875d91b326fa147c99e93652e49
Martin <130224348+mt1nr@users.noreply.github.com>
2023-12-08 10:36:44 +0100

    Update CreateRestaurant.java

--- app/src/main/java/com/example/login/CreateRestaurant.java
+++ app/src/main/java/com/example/login/CreateRestaurant.java
@@ -1,16 +1,23 @@
 package com.example.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -20,7 +27,7 @@ public class CreateRestaurant extends AppCompatActivity {
     TextInputEditText editTextEmail, editTextPassword, editTextStreet;
     Button buttonReg, buttonLog;
     FirebaseAuth mAuth;
-    DatabaseReference dbRef; //Firebase verbinden
+    DatabaseReference dbRef;
     ProgressBar progressBar;
     TextView textView1, textView2;
 
@@ -55,6 +62,51 @@ public class CreateRestaurant extends AppCompatActivity {
                 finish();
             }
         });
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email = String.valueOf(editTextEmail.getText());
+                String password = String.valueOf(editTextPassword.getText());
+                String street = String.valueOf(editTextStreet.getText());
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(CreateRestaurant.this, "Email eingeben", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(CreateRestaurant.this, "Passwort eingeben", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(street)) {
+                    Toast.makeText(CreateRestaurant.this, "Straße eingeben", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(CreateRestaurant.this, "Account erstellt",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplication(), Login.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("AuthError", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(CreateRestaurant.this, "Authentifizierungsfehler: " + task.getException().getMessage(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+            }
+        });
+
     }
 
 
