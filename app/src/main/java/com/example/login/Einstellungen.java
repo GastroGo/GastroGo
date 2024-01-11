db9dffb272a8e3402c810340da7e8fe6862ed145
SchaefferSil <silas@a-schaeffer.de>
2024-01-11 19:21:41 +0100

    skibidi based

--- app/src/main/java/com/example/login/Einstellungen.java
+++ app/src/main/java/com/example/login/Einstellungen.java
@@ -15,7 +15,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
