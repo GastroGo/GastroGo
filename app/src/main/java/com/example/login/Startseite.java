45dfa9b61b718363482fcad7bad056d78fa8ac9b
Martin <schafi350@gmail.com>
2024-01-14 15:31:34 +0100

    vollständig funktionale Suche
    (hab ich auch nicht selber gemacht) (ich kann nicht mehr)

--- app/src/main/java/com/example/login/Startseite.java
+++ app/src/main/java/com/example/login/Startseite.java
@@ -9,6 +9,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,8 +19,10 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -28,6 +31,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -37,6 +41,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Startseite extends AppCompatActivity implements OnMapReadyCallback {
@@ -46,12 +51,17 @@ public class Startseite extends AppCompatActivity implements OnMapReadyCallback 
     FirebaseUser user;
     GoogleMap gMap;
     Marker currentLocationMarker;
+    FloatingActionButton searchButton;
+    SearchView searchView;
+    List<Marker> allMarkers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startseite);
         auth = FirebaseAuth.getInstance();
+        searchButton = findViewById(R.id.search);
+        searchView = findViewById(R.id.searchView);
 
         user = auth.getCurrentUser();
         if (user == null) {
@@ -64,14 +74,46 @@ public class Startseite extends AppCompatActivity implements OnMapReadyCallback 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavigationManager.setupBottomNavigationView(bottomNavigationView, this);
 
-
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchText = searchView.getQuery().toString();
+                searchRestaurants(searchText);
+            }
+        });
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         DropdownManager dropdownManager = new DropdownManager(this, R.menu.dropdown_menu, R.id.imageMenu);
         dropdownManager.setupDropdown();
+    }
 
+    private void searchRestaurants(String searchText) {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Restaurants");
+        dbRef.orderByChild("daten/name").equalTo(searchText).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String restaurantName = snapshot.child("daten/name").getValue(String.class);
+                    Toast.makeText(getApplicationContext(), restaurantName, Toast.LENGTH_LONG).show();
+
+                    // Find the marker for the restaurant
+                    for (Marker marker : allMarkers) {
+                        if (marker.getTitle().equals(restaurantName)) {
+                            // Move camera to restaurant marker
+                            gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 15));
+                            break;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle error here
+            }
+        });
     }
 
     @Override
@@ -116,12 +158,13 @@ public class Startseite extends AppCompatActivity implements OnMapReadyCallback 
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         MyLocationListener locationListener = new MyLocationListener(this, gMap, this);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            gMap.setMyLocationEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+            locationListener.getLastKnownLocation(locationManager);
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-        locationListener.getLastKnownLocation(locationManager);
         addRestaurantsOnMap();
         int padding = 100; // replace with desired padding in pixels
         gMap.setPadding(0, padding, 0, 0);
@@ -157,10 +200,11 @@ public class Startseite extends AppCompatActivity implements OnMapReadyCallback 
                             Location.distanceBetween(currentLocationMarker.getPosition().latitude, currentLocationMarker.getPosition().longitude,
                                     latLng.latitude, latLng.longitude, results);
                             int distanceInMeters = (int) results[0];
-                            gMap.addMarker(new MarkerOptions()
+                            Marker marker = gMap.addMarker(new MarkerOptions()
                                     .position(latLng)
                                     .title(daten.getName())
                                     .snippet("Entfernung: " + distanceInMeters / 1000 + "km"));
+                            allMarkers.add(marker);
                         }
                     }
                 }
