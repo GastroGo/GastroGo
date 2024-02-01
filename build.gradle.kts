a0d401902db889413e9605522d3a6eaa52eb1778
mt1nr <schafi350@gmail.com>
2024-02-01 13:05:15 +0100

    merge fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,9 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-<<<<<<< Updated upstream
-    id("com.android.application") version "8.1.4" apply false
-=======
+
     id("com.android.application") version "8.2.1" apply false
->>>>>>> Stashed changes
+
 
     // Make sure that you have the Google services Gradle plugin dependency
     id("com.google.gms.google-services") version "4.4.0" apply false
