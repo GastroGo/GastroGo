e2ceec99497d05c56fab50b89a7ff6279455d0f8
Martin <schafi350@gmail.com>
2024-05-20 14:32:01 +0200

    animation geht irgenwie nicht
    ich verlier den verstand

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.multidex:multidex:2.0.1") // Add multiDex dependency here
+    implementation("androidx.multidex:multidex:2.0.1")
     implementation("com.google.firebase:firebase-perf:21.0.0")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("com.google.zxing:core:3.5.0")
@@ -60,4 +60,5 @@ dependencies {
     implementation ("com.google.android.gms:play-services-location:21.2.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.vectordrawable:vectordrawable-animated:1.2.0")
 }
---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.login;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.core.splashscreen.SplashScreen;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+}
