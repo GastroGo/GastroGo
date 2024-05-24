843834f199a446441d1b93756462ff1afd0ba354
Martin <schafi350@gmail.com>
2024-05-24 16:19:14 +0200

    fix

--- app/src/main/java/com/example/login/Login.java
+++ app/src/main/java/com/example/login/Login.java
@@ -1,16 +1,22 @@
 package com.example.login;
 
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,15 +27,17 @@ import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.lang.reflect.Field;
+
 public class Login extends AppCompatActivity {
 
     TextInputEditText editTextEmail, editTextPassword;
     Button buttonLogin;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
-    TextView textView;
     InputValidator inputValidator;
-
+    CardView cardView;
+    LinearLayout rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +48,24 @@ public class Login extends AppCompatActivity {
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
         buttonLogin = findViewById(R.id.loginButton);
-
         inputValidator = new InputValidator(this);
-        /*textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplication(), Register.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        cardView = findViewById(R.id.cardView);
+        rootLayout = findViewById(R.id.rootLayout);
 
-         */
+        View.OnFocusChangeListener focusChangeListener = new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    moveCardViewUp();
+                }
+            }
+        };
+
+        editTextEmail.setOnFocusChangeListener(focusChangeListener);
+        editTextPassword.setOnFocusChangeListener(focusChangeListener);
+
+
         //progressBar = findViewById(R.id.progressBar);
-        //textView = findViewById(R.id.registerNow);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,4 +105,24 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+
+    private void moveCardViewUp() {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int screenHeight = displayMetrics.heightPixels;
+
+        int marginInPx = (int) (30 * displayMetrics.density);
+
+        int cardViewHeight = cardView.getHeight();
+        int targetY = (screenHeight / 2) - (cardViewHeight / 2) - marginInPx;
+
+        int cardViewTop = cardView.getTop();
+
+        int translationY = targetY - cardViewTop;
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(cardView, "translationY", translationY);
+        animator.setInterpolator(new DecelerateInterpolator());
+        animator.setDuration(300);
+        animator.start();
+    }
+
 }
