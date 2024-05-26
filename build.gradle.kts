59007893acf8b7e536a05ea74ed6c0d99aeda2ca
Martin <schafi350@gmail.com>
2024-05-26 09:26:04 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,14 @@
-
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.1")
     }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
 
     id("com.android.application") version "8.4.1" apply false
 
 
-    // Make sure that you have the Google services Gradle plugin dependency
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 
     // Add the dependency for the Performance Monitoring Gradle plugin
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
